6fd2b56d8d3fc7d1e7474350c634bb13ee2c94fc
odol0503 <odol0503@users.noreply.github.com>
2017-02-28 09:18:47 +0900

    Update build_ffmpeg_android.sh

--- build_ffmpeg_android.sh
+++ build_ffmpeg_android.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 NDK=/home/shoh/Android/Sdk/ndk-bundle
-SYSROOT=$NDK/platforms/android-9/arch-arm/
-TOOLCHAIN=$NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64
+SYSROOT=$NDK/platforms/android-21/arch-arm64/
+TOOLCHAIN=$NDK/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64
 function build_one
 {
 	./configure \
 		--prefix=$PREFIX \
-		--extra-cflags="-Os -fpic $ADDI_CFLAGS -I./libx264/include"
+		--extra-cflags="-Os -fpic -I./libx264/include" \
 		--extra-ldflags=-L./libx264/lib \
 		--enable-gpl \
 		--enable-libx264 \
@@ -21,9 +21,9 @@ function build_one
 		--disable-avdevice \
 		--disable-doc \
 		--disable-symver \
-		--cross-prefix=$TOOLCHAIN/bin/arm-linux-androideabi- \
+		--arch=aarch64 \
+		--cross-prefix=$TOOLCHAIN/bin/aarch64-linux-android- \
 		--target-os=linux \
-		--arch=arm \
 		--enable-cross-compile \
 		--sysroot=$SYSROOT \
 		$ADDITIONAL_CONFIGURE_FLAG
@@ -33,6 +33,4 @@ function build_one
 }
 CPU=arm
 PREFIX=$(pwd)/android/$CPU
-ADDI_CFLAGS="-marm"
 build_one
-
